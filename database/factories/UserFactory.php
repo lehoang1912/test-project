281f84eb9abb985e04b363cf30f453f4f10e78a8
Hoang Le <>
2020-12-12 22:40:54 +0700

    fix default password for seeds

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
             'first_name' => $this->faker->firstName,
             'last_name' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password',
             'role' => User::ROLE_ENGINEER
         ];
     }
